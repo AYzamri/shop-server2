bd3ed5e67e4958f377becc5da7beb05056c9d700
amitp <amitpeer05@gmail.com>
2017-06-27 17:26:14 +0300

    shoping cart

--- public/ShoppingCart.js
+++ public/ShoppingCart.js
@@ -1,12 +1,22 @@
 //-------------------------------------------------------------------------------------------------------------------
-angular.module('myApp').controller('cartController', ['CartService', '$location', '$window',
+app.controller('cartController', ['CartService', '$location', '$window',
     function (CartService, $location, $window) {
         let self = this;
         self.cartService = CartService;
 
+        self.seeRecordDetails = function (record) {
+            self.recordToSee = record;
+            self.toggleModal();
+        };
+
+        self.modalShown = false;
+        self.toggleModal = function() {
+            self.modalShown = !self.modalShown;
+        };
+
     }]);
 //-------------------------------------------------------------------------------------------------------------------
-angular.module('myApp').factory('CartService', ['$http', function ($http) {
+app.factory('CartService', ['$http', function ($http) {
     let service = {};
 
     // this is the shopping cart which will be presented in cart.html
@@ -22,10 +32,14 @@ angular.module('myApp').factory('CartService', ['$http', function ($http) {
         console.log("CartService::addRecordToCart");
 
         service.cart.push({
-            id: record.RecordID,
-            name: record.Name,
-            artist: record.Artist,
-            price: record.Price
+            RecordID: record.RecordID,
+            Name: record.Name,
+            Artist: record.Artist,
+            Price: record.Price,
+            ReleasedYear: record.ReleasedYear,
+            Description: record.Description,
+            PicturePath: record.PicturePath,
+            ArriveDateInStore: record.ArriveDateInStore
         });
         alert("'" + record.Name + "' " + "has been added to your cart")
         service.priceSum += record.Price;
@@ -49,7 +63,7 @@ angular.module('myApp').factory('CartService', ['$http', function ($http) {
         console.log("CartService::recordIndexInCart");
 
         for (var i = 0; i < service.cart.length; i += 1) {
-            if (service.cart[i]['id'] === record.RecordID) {
+            if (service.cart[i]['RecordID'] === record.RecordID) {
                 return i;
             }
         }
@@ -58,3 +72,25 @@ angular.module('myApp').factory('CartService', ['$http', function ($http) {
 
     return service;
 }]);
+//-------------------------------------------------------------------------------------------------------------------
+app.directive('modalDialog', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            show: '='
+        },
+        replace: true, // Replace with the template below
+        transclude: true, // we want to insert custom content inside the directive
+        link: function(scope, element, attrs) {
+            scope.dialogStyle = {};
+            if (attrs.width)
+                scope.dialogStyle.width = attrs.width;
+            if (attrs.height)
+                scope.dialogStyle.height = attrs.height;
+            scope.hideModal = function() {
+                scope.show = false;
+            };
+        },
+        template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+    };
+});
